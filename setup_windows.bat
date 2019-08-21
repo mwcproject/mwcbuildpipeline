08ae3d744855beda60653a36e0254ea6b4af9326
cgilliard <christopher.gilliard@gmail.com>
2019-08-21 16:35:36 -0700

    fix win64 stuff

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,3 +1,9 @@
-cat Qt5.tar.bz2.* > ./Qt.tar.bz2
-bzip2 -dc Qt.tar.bz2 | tar xvf -
-del /q /s Qt.tar.bz2
+
+git clone https://github.com/mwcproject/mwcbuilder-win64-helpers
+
+cat mwcbuilder-win64-helpers/Qt5.tar.bz2.* | bzip2 -dc | tar xvf -
+bzip2 -dc mwcbuilder-win64-helpers/libs.tar.bz2 | tar xvf -
+cp mwcbuilder-win64-helpers/pscp.exe .
+
+rm -rf mwcbuilder-win64-helpers
+
