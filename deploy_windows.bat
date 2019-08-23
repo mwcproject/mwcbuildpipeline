6520b61dd80986812eb94355708ece5387f046b3
Christopher Gilliard <Christopher.Gilliard@gmail.com>
2019-08-23 01:11:05 +0000

    print dir

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -13,5 +13,6 @@ for /f "skip=1 tokens=1-6 delims= " %%a in ('wmic path Win32_LocalTime Get Day^,
 )
 
 set NAME=mwc-qt-wallet-1.0-5.beta.%1-win64-setup.exe
+ls -l target\nsis
 copy target\nsis\mwc-qt-wallet-1.0-5-win64-setup.exe target\nsis\%NAME%
 echo y | pscp -pw uploader target\nsis\%NAME% uploader@ftp.mwc.mw:/home/uploader/
