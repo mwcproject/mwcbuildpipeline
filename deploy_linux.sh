04f55d6ade2256ef9dc42474c9c5170ef4973565
Christopher Gilliard <Christopher.Gilliard@gmail.com>
2019-08-23 00:14:36 +0000

    make name more parsable

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-DPKG_NAME=mwc-qt-wallet-1.0-5.beta.$1
+DPKG_NAME=mwc-qt-wallet_1.0-5.beta.$1
 echo "md5sum = `md5sum target/*.deb`";
 mkdir -p ~/.ssh
 echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
 DATE=`date +"%m-%d-%y"`
 ./sendfile.expect target/*.deb $DPKG_NAME-linux64-$DATE.deb;
-./sendfile.expect target/*.tar.gz $DPKG_NAME-linux64-$DATE.tar.gz;
+./sendfile.expect target/*.tar.gz $DPKG_NAME_linux64-$DATE.tar.gz;
 
