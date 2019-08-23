680611447820e6e54ffc06231ea83ad75f1247c5
Christopher Gilliard <Christopher.Gilliard@gmail.com>
2019-08-23 02:17:58 +0000

    add quotes around var

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -7,5 +7,5 @@ mkdir -p ~/.ssh
 echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
 DATE=`date +"%m-%d-%y"`
 ./sendfile.expect target/*.deb $DPKG_NAME-linux64-$DATE.deb;
-./sendfile.expect target/*.tar.gz $DPKG_NAME_linux64-$DATE.tar.gz;
+./sendfile.expect target/*.tar.gz "$DPKG_NAME"_linux64-$DATE.tar.gz;
 
