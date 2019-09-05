84d082f2f406c4b2d813312cd78e1065cdf7f723
cgilliard <christopher.gilliard@gmail.com>
2019-09-04 23:01:30 -0400

    new upload for mac

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -2,7 +2,5 @@
 
 
 echo "md5sum = `md5 target/*.dmg`";
-mkdir -p ~/.ssh
-echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
-DATE=`date +"%m-%d-%y"`
-./sendfile.expect target/*.dmg mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg
+cp target/*.dmg mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg;
+scp -i ./uploader.pem mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg uploader\@3.228.53.68:/home/uploader/
