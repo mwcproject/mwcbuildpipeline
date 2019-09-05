49bb0863c4fcb8650839b24424e7836ce6f12f1d
cgilliard <christopher.gilliard@gmail.com>
2019-09-04 21:55:48 -0400

    upload to aws linux

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -6,6 +6,8 @@ echo "md5sum = `md5sum target/*.deb`";
 mkdir -p ~/.ssh
 echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
 DATE=`date +"%m-%d-%y"`
-./sendfile.expect target/*.deb $DPKG_NAME-linux64-$DATE.deb;
-./sendfile.expect target/*.tar.gz "$DPKG_NAME"_linux64-$DATE.tar.gz;
+cp target/*.deb $DPKG_NAME-linux64-$DATE.deb
+cp target/*.tar.gz $DPKG_NAME_linux64-$DATE.tar.gz
+scp -i ./uploader.pem $DPKG_NAME-linux64-$DATE.deb uploader@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem $DPKG_NAME_linux64-$DATE.tar.gz uploader@3.228.53.68:/home/uploader/
 
