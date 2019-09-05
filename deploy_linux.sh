d8e23fb6180cf67fd3d834001ee7fde7e73c5d90
cgilliard <christopher.gilliard@gmail.com>
2019-09-04 23:19:04 -0400

    try with no strict checking

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -7,6 +7,6 @@ mkdir -p ~/.ssh
 DATE=`date +"%m-%d-%y"`
 cp target/*.deb $DPKG_NAME-linux64-$DATE.deb
 cp target/*.tar.gz "$DPKG_NAME"_linux64-$DATE.tar.gz
-scp -i ./uploader.pem $DPKG_NAME-linux64-$DATE.deb uploader\@3.228.53.68:/home/uploader/
-scp -i ./uploader.pem "$DPKG_NAME"_linux64-$DATE.tar.gz uploader\@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' $DPKG_NAME-linux64-$DATE.deb uploader\@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' "$DPKG_NAME"_linux64-$DATE.tar.gz uploader\@3.228.53.68:/home/uploader/
 
