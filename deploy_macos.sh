9b15ffe80f4be5fd176448ae81ecb0ae8daa89df
cgilliard <christopher.gilliard@gmail.com>
2019-09-04 23:20:28 -0400

    fix on mac

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -3,4 +3,4 @@
 
 echo "md5sum = `md5 target/*.dmg`";
 cp target/*.dmg mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg;
-scp -i ./uploader.pem mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg uploader\@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' mwc-qt-wallet-1.0-5.beta.$1-macosx.dmg uploader\@3.228.53.68:/home/uploader/
