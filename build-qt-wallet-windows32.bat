8e03218861b5be373222d9f67f4e191fdc782235
Christoher Gilliard <christopher.gilliard@gmail.com>
2019-10-02 08:25:54 +0000

    fix bat scripts

--- build-qt-wallet-windows32.bat
+++ build-qt-wallet-windows32.bat
@@ -15,10 +15,10 @@ mkdir target
 
 git clone https://github.com/mwcproject/mwc713
 cd mwc713
-set TAG_FOR_BUILD_FILE=%cd%\..\mwc713.version
-IF EXIST "$TAG_FOR_BUILD_FILE" (
+set TAG_FOR_BUILD_FILE=..\mwc713.version
+IF EXIST "%TAG_FOR_BUILD_FILE%" (
     git fetch && git fetch --tags;
-    git checkout `cat $TAG_FOR_BUILD_FILE`;
+    git checkout `cat %TAG_FOR_BUILD_FILE%`;
 )
 cargo build --release
 cd ..
@@ -29,11 +29,11 @@ set PATH=%cd%\Qt\Tools\mingw730_32\bin;%cd%\Qt\5.13.0\mingw73_32\bin;%PATH%
 
 git clone https://github.com/mwcproject/mwc-qt-wallet
 cd mwc-qt-wallet
-set TAG_FOR_BUILD_FILE=%cd%\..\mwc-qt-wallet.version
-IF EXIST "$TAG_FOR_BUILD_FILE" (
+set TAG_FOR_BUILD_FILE=..\mwc-qt-wallet.version
+IF EXIST "%TAG_FOR_BUILD_FILE%" (
     git fetch && git fetch --tags;
-    git checkout `cat $TAG_FOR_BUILD_FILE`;
-    echo #define BUILD_VERSION "`cat $TAG_FOR_BUILD_FILE`" > build_version.h
+    git checkout `cat %TAG_FOR_BUILD_FILE%`;
+    echo #define BUILD_VERSION "`cat %TAG_FOR_BUILD_FILE%`" > build_version.h
 )
 else (
     echo #define BUILD_VERSION "1.0-6.beta.%1" > build_version.h
