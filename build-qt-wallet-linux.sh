15cbdb91a6b91fc431c465be0f4713e14843f433
Cloud User <centos@ip-172-26-3-118.ec2.internal>
2019-10-03 04:29:42 +0000

    better tar and debian packages

--- build-qt-wallet-linux.sh
+++ build-qt-wallet-linux.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+VERSION=1.0-6.beta.$1
+TAG_FOR_BUILD_FILE=mwc-qt-wallet.version
+if [ -f "$TAG_FOR_BUILD_FILE" ]; then
+VERSION=`cat $TAG_FOR_BUILD_FILE`;
+fi
+
 # Clean everything.
 rm -rf mwc713 mwc-qt-wallet target/*
 mkdir -p target
@@ -33,7 +39,7 @@ if [ -f "$TAG_FOR_BUILD_FILE" ]; then
     git checkout `cat $TAG_FOR_BUILD_FILE`;
     echo "#define BUILD_VERSION  \"`cat $TAG_FOR_BUILD_FILE`\"" > build_version.h
 else
-    echo "#define BUILD_VERSION  \"1.0-6.beta.$1\"" > build_version.h
+    echo "#define BUILD_VERSION  \"$VERSION\"" > build_version.h
 fi
 
 ../Qt/5.9/gcc_64/bin/qmake mwc-qt-wallet.pro QMAKE_CXXFLAGS="-fno-sized-deallocation -pipe" -config release -spec linux-g++ CONFIG+=x86_64
@@ -53,7 +59,7 @@ if [ ! -f "$FILE" ]; then
 fi
 
 cd ../
-export DPKG_VERSION=1.0-5.beta.$1
+export DPKG_VERSION=$VERSION
 export DPKG_NAME=mwc-qt-wallet-$DPKG_VERSION
 echo "Building $DPKG_NAME"
 mkdir -p target/$DPKG_NAME/usr/local/bin/
@@ -76,15 +82,16 @@ echo "Building debain package at target/$DPKG_NAME.deb"
 dpkg-deb --build $DPKG_NAME
 
 echo "Building tar.gz"
-mkdir -p tmp/mwc-qt-wallet-1.0
-cp ../mwc-qt-wallet/mwc-qt-wallet tmp/mwc-qt-wallet-1.0/mwc-qt-wallet.bin
-cp ../mwc713/target/release/mwc713 tmp/mwc-qt-wallet-1.0
-cp ../resources/mwc-qt-wallet.tarver.sh tmp/mwc-qt-wallet-1.0/mwc-qt-wallet
-cp ../resources/mwc-qt-wallet_lr.tarver.sh tmp/mwc-qt-wallet-1.0/mwc-qt-wallet_lr
+QT_WALLET_DIRECTORY=tmp/mwc-qt-wallet-$VERSION
+mkdir -p tmp/mwc-qt-wallet-$VERSION
+cp ../mwc-qt-wallet/mwc-qt-wallet $QT_WALLET_DIRECTORY/mwc-qt-wallet.bin
+cp ../mwc713/target/release/mwc713 $QT_WALLET_DIRECTORY
+cp ../resources/mwc-qt-wallet.tarver.sh $QT_WALLET_DIRECTORY/mwc-qt-wallet
+cp ../resources/mwc-qt-wallet_lr.tarver.sh $QT_WALLET_DIRECTORY/mwc-qt-wallet_lr
 
 cd tmp
-tar cvf ../mwc-qt-wallet-1.0-5.tar mwc-qt-wallet-1.0
-gzip ../mwc-qt-wallet-1.0-5.tar
+tar cvf ../mwc-qt-wallet-$VERSION.tar mwc-qt-wallet-$VERSION
+gzip ../mwc-qt-wallet-$VERSION.tar
 
 echo "Build Complete";
 
