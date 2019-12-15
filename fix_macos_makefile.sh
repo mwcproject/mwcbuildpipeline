d5d0a62829d506c44f9d7533d35599934360728a
kbay <kbay@vmware.com>
2019-12-14 18:26:23 -0800

    MacOS build, update dlls path. Add one more.

--- fix_macos_makefile.sh
+++ fix_macos_makefile.sh
@@ -11,7 +11,7 @@ cat Makefile.replace >> Makefile.replace2
 # Update libs
 sed 's/^LIBS.*/LIBS          = -F\$\(QT_DIR\)\/5.9\/clang_64\/lib -framework AppKit -framework QtSvg -framework QtWidgets -framework QtGui -framework QtCore -framework DiskArbitration -framework IOKit -framework QtNetwork -framework OpenGL -framework AGL/' Makefile.replace2 > Makefile.replace3
 
-sed 's/^LFLAGS.*/LFLAGS        = -stdlib=libc++ -headerpad_max_install_names \$\(EXPORT_ARCH_ARGS\) -Wl,-syslibroot,\/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/MacOSX.platform\/Developer\/SDKs\/MacOSX10.14.sdk -mmacosx-version-min=10.10 -Wl,-rpath,@executable_path\/Frameworks -Wl,-rpath,\$\(QT_DIR\)\/5.9\/clang_64\/lib/' Makefile.replace3 > Makefile.replace4
+sed 's/^LFLAGS.*/LFLAGS        = -stdlib=libc++ -headerpad_max_install_names \$\(EXPORT_ARCH_ARGS\) -Wl,-syslibroot,\/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/MacOSX.platform\/Developer\/SDKs\/MacOSX.sdk -mmacosx-version-min=10.10 -Wl,-rpath,@executable_path\/..\/Frameworks -Wl,-rpath,@executable_path\/Frameworks -Wl,-rpath,\$\(QT_DIR\)\/5.9\/clang_64\/lib/' Makefile.replace3 > Makefile.replace4
 
 # Delete plugin_import.o from linking
 cp Makefile.replace4 Makefile.replace5
