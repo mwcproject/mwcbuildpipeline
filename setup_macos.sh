19c3ec15377c45e5341f9720aa4bf49b9e5775be
kbay <kbay@vmware.com>
2019-12-14 16:54:03 -0800

    MacOS build fix2

--- setup_macos.sh
+++ setup_macos.sh
@@ -6,17 +6,15 @@ echo "##vso[task.setvariable variable=PATH;]$PATH:$HOME/.cargo/bin"
 
 # Get helper files
 git clone https://github.com/mwcproject/mwcbuilder-macos-helpers
-cat mwcbuilder-macos-helpers/qt_598_* | bzip2 -dc | tar xvf -
+cat mwcbuilder-macos-helpers/qt_59_* | bzip2 -dc | tar xvf -
 rm -rf mwcbuilder-linux-helpers
 
 # Need to fix what installer did. Installer hardecoded paths to libs by some reasons and it is breaks the build
 echo "Patching QT paths. MacOS issue"
-grep -rl kbay . | grep prl | xargs sed -i '' 's/-F\/Users\/kbay\/Qt\/5.9.8\/clang_64\/lib//g'
+grep -rl kbay . | grep prl | xargs sed -i '' 's/-F\/Users\/kbay\/Qt\/5.9\/clang_64\/lib//g'
 # It is easy to check if you need that patch:
 # 1. Remove the sed command
 # 2. Try to build
 # 3. Check logs if they have your local paths. If you see them - it is a problem for azure.
 
-# If is what make fix expect to see
-mv Qt/5.9.8  Qt/5.9
 
