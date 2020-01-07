e432f45835ea7a21abfa5a854e497d3be9c87b6f
cgilliard <christopher.gilliard@gmail.com>
2020-01-06 17:01:25 -0800

    fix checksum

--- deploy_windows32.bat
+++ deploy_windows32.bat
@@ -1,7 +1,7 @@
 setlocal enableextensions enabledelayedexpansion
 
 echo "Param passed in is %1"
-md5sum target\nsis\mwc-qt-wallet*-setup.exe
+md5sum target\\nsis\\mwc-qt-wallet*-setup.exe
 mkdir %systemdrive%%homepath%\.ssh
 echo ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM= >> %systemdrive%%homepath%\.ssh\known_hosts
 
