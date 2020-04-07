4cf15f65b7e5246b5362669b3bf2ebef29d9c332
cgilliard <christopher.gilliard@gmail.com>
2020-04-06 19:14:28 -0700

    beta

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -7,7 +7,7 @@ if [ -f "$TAG_FOR_BUILD_FILE" ]; then
 VERSION=`cat $TAG_FOR_BUILD_FILE`
 DPKG_NAME=mwc-qt-wallet_$VERSION
 else
-DPKG_NAME=mwc-qt-wallet_1.0-15.beta.$1
+DPKG_NAME=mwc-qt-wallet_1.0-16.beta.$1
 fi
 echo "md5sum = `md5sum target/*.deb`";
 mkdir -p ~/.ssh
