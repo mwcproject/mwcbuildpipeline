a099841c96775a90d8470961c774c6b4c458b58a
bayk <konstantin.bay@gmail.com>
2020-04-09 11:38:56 -0700

    linux deployment package names

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -13,7 +13,9 @@ echo "md5sum = `md5sum target/*.deb`";
 mkdir -p ~/.ssh
 cp target/*.deb $DPKG_NAME-linux64-$CPU_PACKAGE_NAME.deb
 cp target/*.tar.gz "$DPKG_NAME"_linux64_$CPU_PACKAGE_NAME.tar.gz
-scp -i ./uploader.pem -o 'StrictHostKeyChecking no' $DPKG_NAME-linux64.deb uploader\@3.228.53.68:/home/uploader/
-scp -i ./uploader.pem -o 'StrictHostKeyChecking no' "$DPKG_NAME"_linux64.tar.gz uploader\@3.228.53.68:/home/uploader/
-scp -i ./uploader.pem -o 'StrictHostKeyChecking no' ~/rpmbuild/RPMS/x86_64/*.rpm uploader\@3.228.53.68:/home/uploader/
+cp ~/rpmbuild/RPMS/*.rpm "$DPKG_NAME"_linux64_$CPU_PACKAGE_NAME.rpm
+
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' $DPKG_NAME-linux64_$CPU_PACKAGE_NAME.deb uploader\@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' $DPKG_NAME_linux64_$CPU_PACKAGE_NAME.tar.gz uploader\@3.228.53.68:/home/uploader/
+scp -i ./uploader.pem -o 'StrictHostKeyChecking no' "$DPKG_NAME"_linux64_$CPU_PACKAGE_NAME.rpm uploader\@3.228.53.68:/home/uploader/
 
