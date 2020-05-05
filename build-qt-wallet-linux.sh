4f978a36ad17e7b598431cd98147de452a7553a4
cgilliard <christopher.gilliard@gmail.com>
2020-05-05 11:02:47 -0700

    release build 1.0.17

--- build-qt-wallet-linux.sh
+++ build-qt-wallet-linux.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VERSION=1.0-16.beta.$1
+VERSION=1.0-17.beta.$1
 VERSION_NAME=1.0
-RELEASE_NAME=16.beta.$1
+RELEASE_NAME=17.beta.$1
 TAG_FOR_BUILD_FILE=mwc-qt-wallet.version
 if [ -f "$TAG_FOR_BUILD_FILE" ]; then
     VERSION=`cat $TAG_FOR_BUILD_FILE`;
     VERSION_NAME=1.0
-    RELEASE_NAME=16
+    RELEASE_NAME=17
 fi
 
 # Clean everything.
