4781d864fd2a1b72761b3cf60b877a8a02ad3339
cgilliard <christopher.gilliard@gmail.com>
2020-06-04 12:46:41 -0700

    don't upload version.txt

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -3,13 +3,7 @@ df -h
 rem need putty for scp
 choco install -y putty
 
-echo "scp.expect version.txt %1"
-echo n | pscp -scp -pw %1 version.txt uploader@3.228.53.68:/home/uploader/
-
 git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
-
-echo "scp.expect version.txt %1"
-echo n | pscp -scp -pw %1 version.txt uploader@3.228.53.68:/home/uploader/
 
 cat mwcbuilder-win32-helpers/qt5-part2/* | bzip2 -dc | tar xvf -
 cat mwcbuilder-win32-helpers/qt5/* | bzip2 -dc | tar xvf -
@@ -17,13 +11,9 @@ bzip2 -dc mwcbuilder-win32-helpers/libs32.tar.bz2 | tar xvf -
 
 rm -rf mwcbuilder-win32-helpers
 
-echo "scp.expect version.txt %1"
-echo n | pscp -scp -pw %1 version.txt uploader@3.228.53.68:/home/uploader/
-
 choco install -y llvm
 
 rustup install stable-i686-pc-windows-msvc
-
 
 rem rustup install 1.37.0-i686-pc-windows-msvc
 rem rustup override set 1.37.0-i686-pc-windows-msvc
