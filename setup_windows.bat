fa22fb79860c16ef8644153488d42c64857274fe
cgilliard <christopher.gilliard@gmail.com>
2020-06-04 11:17:03 -0700

    add pscp

--- setup_windows.bat
+++ setup_windows.bat
@@ -2,7 +2,7 @@
 git clone https://github.com/mwcproject/mwcbuilder-win64-helpers
 
 echo "scp.expect version.txt %1"
-pscp -pw %1 version.txt uploader@3.228.53.68:/home/uploader
+pscp.exe -pw %1 version.txt uploader@3.228.53.68:/home/uploader
 
 cat mwcbuilder-win64-helpers/Qt5.tar.bz2.* | bzip2 -dc | tar xvf -
 bzip2 -dc mwcbuilder-win64-helpers/libs.tar.bz2 | tar xvf -
@@ -10,7 +10,7 @@ bzip2 -dc mwcbuilder-win64-helpers/libs.tar.bz2 | tar xvf -
 rm -rf mwcbuilder-win64-helpers
 
 echo "scp.expect version.txt %1"
-pscp -pw %1 version.txt uploader@3.228.53.68:/home/uploader
+pscp.exe -pw %1 version.txt uploader@3.228.53.68:/home/uploader
 
 choco install -y llvm
 choco install rust
