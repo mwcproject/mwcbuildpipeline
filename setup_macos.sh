3918dda5dde4c47a8e010dd0f8cfcea267d97ab8
cgilliard <christopher.gilliard@gmail.com>
2020-06-04 09:09:58 -0700

    use credentials for linux

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo "1=$1";
-echo "2=$2";
-UPOADCREDS=$1
-echo "creds=$UPLOADCREDS"
-
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 # ~/.cargo/bin/rustup override set 1.37.0
 echo "##vso[task.setvariable variable=PATH;]$PATH:$HOME/.cargo/bin"
@@ -14,9 +9,6 @@ git clone https://github.com/mwcproject/mwcbuilder-macos-helpers
 cat mwcbuilder-macos-helpers/qt_59_* | bzip2 -dc | tar xvf -
 rm -rf mwcbuilder-macos-helpers
 
-echo "1=$1";
-echo "2=$2";
-echo "creds=$UPLOADCREDS"
 brew uninstall llvm
 
 # Need to fix what installer did. Installer hardecoded paths to libs by some reasons and it is breaks the build
