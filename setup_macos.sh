82e47f7302c53d7c9be6aea6bd69f628c77037e9
electrum17 <electrum17@protonmail.com>
2020-06-15 07:19:04 -0700

    more keychains

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-ls -l ~/Library/Keychains/
+# setup certs
+ls -l ~/Library/Keychains
+openssl enc -d -aes-256-cbc -in certs.tar.gz.enc -out certs.tar.gz -k $1
+gzip -dc certs.tar.gz | tar xvf -
+
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain certs/developerID_installer.cer
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain certs/developerID_application.cer
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login_renamed_1.keychain certs/developerID_installer.cer
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login_renamed_1.keychain certs/developerID_application.cer
+
+cp certs/mimblewimble* ~/Library/Keychains
+ls -l ~/Library/Keychains
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 # ~/.cargo/bin/rustup override set 1.37.0
@@ -20,14 +31,4 @@ grep -rl kbay . | grep prl | xargs sed -i '' 's/-F\/Users\/kbay\/Qt\/5.9\/clang_
 # 1. Remove the sed command
 # 2. Try to build
 # 3. Check logs if they have your local paths. If you see them - it is a problem for azure.
-
-# setup certs
-openssl enc -d -aes-256-cbc -in certs.tar.gz.enc -out certs.tar.gz -k $1
-gzip -dc certs.tar.gz | tar xvf -
-
-sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain certs/developerID_installer.cer
-sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain certs/developerID_application.cer
-
-cp certs/mimblewimble* ~/Library/Keychains
-ls -l ~/Library/Keychains
 
