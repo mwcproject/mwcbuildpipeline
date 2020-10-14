641fdc4e1f2c837d46e66ac5d407f672d6ac5d3c
Jim Byrer <jbyrer@gmail.com>
2020-10-13 20:54:44 -0700

    more debug

--- build-qt-wallet-macos.sh
+++ build-qt-wallet-macos.sh
@@ -99,10 +99,11 @@ else
    openssl enc -d -aes-256-cbc -in ../certsJB.tar.gz.enc -out certs.tar.gz -k $3
    echo "About to unpack certs"
    pwd
+   ls -alt
    ls -alt certs
    #gzip -dc certs.tar.gz | tar xvf -
    tar -xvf certs.tar.gz
-
+   echo "finished untar?"
    sudo security create-keychain -p password nchain.keychain
    sudo security add-certificates -k nchain.keychain certs/azure_cert.cer
    sudo security unlock-keychain -p password nchain.keychain
