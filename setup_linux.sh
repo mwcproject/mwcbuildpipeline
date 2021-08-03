3bf824542b0ebea3d8a7502b768a37b8d7a63f65
bayk <konstantin.bay@gmail.com>
2021-08-03 10:28:39 -0700

    Fixing build

--- setup_linux.sh
+++ setup_linux.sh
@@ -6,10 +6,9 @@ sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6
 
 # Add deps
 sudo apt update
-sudo apt-get install expect
-sudo apt install clang-8 git curl make build-essential mesa-utils libgl1-mesa-dev openssl libssl-dev -y
-
 sudo apt-get update -yqq
+sudo apt-get purge --auto-remove clang-12 clang-format-12 llvm-12 llvm-12-dev llvm-12-tools llvm-12-runtime clang-11 clang-format-11 llvm-11 llvm-11-dev llvm-11-tools llvm-11-runtime clang-10 clang-format-10 llvm-10 llvm-10-dev llvm-10-tools llvm-10-runtime clang-9 clang-format-9 llvm-9 llvm-9-dev llvm-9-tools llvm-9-runtime
+sudo apt install clang-8 git curl make build-essential mesa-utils libgl1-mesa-dev openssl libssl-dev -y
 sudo apt-get install -yqq --no-install-recommends libncursesw5-dev
 
 # list what we have
