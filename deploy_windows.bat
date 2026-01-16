a520c7c5be3465786f9a1ef23bfa35616431105f
bay <noname@noname.com>
2026-01-15 16:05:23 -0800

    Windows upload versions mismatch

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -21,8 +21,8 @@ echo "version = !VERSION!"
 set NAME=mwc-qt-wallet-!VERSION!-win64-setup.exe
 set NAME_UPLOAD=mwc-qt-wallet_!VERSION!-win64-setup-%CPU_PACKAGE_NAME%.exe
 ) ELSE (
-set NAME=mwc-qt-wallet-1.2.!NUMBER_GLOBAL!.beta.%1-win64-setup.exe
-set NAME_UPLOAD=mwc-qt-wallet_1.2.!NUMBER_GLOBAL!.beta.%1-win64-setup-%CPU_PACKAGE_NAME%.exe
+set NAME=mwc-qt-wallet-2.0.!NUMBER_GLOBAL!.beta.%1-win64-setup.exe
+set NAME_UPLOAD=mwc-qt-wallet_2.0.!NUMBER_GLOBAL!.beta.%1-win64-setup-%CPU_PACKAGE_NAME%.exe
 )
 echo "Using %NAME% and %NAME_UPLOAD%"
 
